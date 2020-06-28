e36b9fa02ca57711a8604a4ebe6fd2b8e06df631
johnv87 <johnv87@users.noreply.github.com>
2020-06-28 18:09:31 +0200

    Updates

---
+++ precache-manifest.d1940bef75d0dc84499cea0b0d93c95d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03370db9d680c86274619ba3132f399",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2d23222c4bede629db1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9028f1b0cca8a1da5de6",
+    "url": "/static/js/2.635734e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.635734e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d23222c4bede629db1",
+    "url": "/static/js/main.6e540281.chunk.js"
+  },
+  {
+    "revision": "5703821851999ea2ecbc",
+    "url": "/static/js/runtime-main.7541d37a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
